c9b71a1dfe60821f0bac622fcbd21ccf5ccbee68
SanaIshaqat <sanaishaqat@gmail.com>
2021-11-08 20:39:18 +0200

    lab07

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # bearer-auth
+## LAB - 07
+### Authentication 
+Author: Sana Ishaqat
+
+[tests report (actions)](https://github.com/SanaIshaqat/bearer-auth/actions)
+
+[back-end (heroku proof of life)](https://sana-basic-auth-401.herokuapp.com/alive)
+
+[back-end (non-exist routes)](https://sana-basic-auth-401.herokuapp.com/bla)
+[back-end (Error)](https://sana-basic-auth-401.herokuapp.com/error)
+
+[PR Link](https://github.com/SanaIshaqat/basic-auth/pull/1)
+
+[Final PR](https://github.com/SanaIshaqat/basic-auth/pull/3)
+### Setup
+.env requirements
+PORT - Port Number
+
+
+### Running the app
+npm run dev
+Endpoint: /status
+Returns Object
+{
+  "status": "running",
+  "port": 3030,
+  "domain": "https://sana-basic-auth-401.herokuapp.com"
+}
+
+### Tests
+Unit Tests: npm run test
+
+![](Lab07PassedTests.PNG)
+### ThunderClientTests
+
+![](signInEndPoint.PNG)
+
+![](signUpEndPoint.PNG)
+
+![](usersEndPoint.PNG)
+
+![](secretEndPoint.PNG)
---
+++ src/auth/models/users.js
@@ -0,0 +1,65 @@
+'use strict';
+
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const SECRET = process.env.SECRET ;
+
+
+
+
+const userSchema = (sequelize, DataTypes) => {
+  const model = sequelize.define('new_users', {
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    token: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return jwt.sign({ username: this.username }, SECRET);
+      },
+      set(tokenObj) {
+        let token = jwt.sign(tokenObj, SECRET);
+        return token;
+      }
+    },
+    
+    
+  });
+
+  model.beforeCreate(async (user) => {
+    let hashedPass =await bcrypt.hash(user.password, 10);
+    user.password = hashedPass;
+     return hashedPass
+  });
+
+  // Basic AUTH: Validating strings (username, password) 
+  model.authenticateBasic = async function (username, password) {
+    const user = await this.findOne({ where: {username: username}  })
+    console.log('this user :',user);
+    console.log("password :", password);
+    console.log("user.password : ", user.password)
+    const valid = await bcrypt.compare(password, user.password)
+    console.log("valid : ", valid);
+    if (valid) { 
+    
+        return (user)
+    }
+    throw new Error('Invalid User');
+  }
+
+  // Bearer AUTH: Validating a token
+  model.authenticateToken = async function (token) {
+    try {
+      const parsedToken = jwt.verify(token, SECRET);
+      const user = await this.findOne({ where: {username:parsedToken.username} })
+      if (user) { return user; }
+      throw new Error("User Not Found");
+    } catch (e) {
+      throw new Error(e.message)
+    }
+  }
+
+  return model;
+}
+
+module.exports = userSchema;
