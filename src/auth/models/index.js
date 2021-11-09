74ea5a35499917add2bfd6ea070d3ccd25e5d4d2
Sana Ishaqat <86602001+SanaIshaqat@users.noreply.github.com>
2021-11-09 22:20:11 +0200

    Update index.js

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -6,12 +6,13 @@ const userSchema = require('./users.js');
 
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;
 
-const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
-  dialectOptions: {
-    ssl: true,
-    rejectUnauthorized: false,
-  }
-} : {}
+const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? { 
+  dialectOptions: { 
+    ssl: { 
+      require: true, 
+      rejectUnauthorized: false 
+    } 
+  } } : {}
 
 const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
 
